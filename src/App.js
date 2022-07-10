6c6e710583cabe192953796bb864adb787f87945
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-07-10 17:57:29 -0400

    added title change functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 const App = () => {
   const [searchField, setSearchField] = useState(''); // [value, setValue]
   // console.log( searchField );
+  const [title, setTitle] = useState('');
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
   // const [stringField, setStringField] = useState('');
@@ -36,6 +37,12 @@ const App = () => {
       setSearchField(searchFieldString);
     }
 
+    const onTitleChange = (event) => {
+      // console.log({startingArray: this.state.monsters});
+      const searchFieldString = event.target.value.toLocaleLowerCase();
+      setTitle(searchFieldString);
+    }
+
   // const onStringChange = (event) => {
   //   setStringField(event.target.value)
   // }
@@ -45,11 +52,19 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'>{title}</h1>
       <SearchBox
           className = "monsters-search-box"
           onChangeHandler={onSearchChange}
           placeholder="search monsters"
+      />
+
+      <br />
+
+      <SearchBox
+          className = "title-search-box"
+          onChangeHandler={onTitleChange}
+          placeholder="set title"
       />
 
       {/* <SearchBox
